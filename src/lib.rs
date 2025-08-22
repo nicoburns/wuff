f7cffd9081175475dad60aed0f07b8da277c96cc
Nico Burns <nico@nicoburns.com>
2025-08-22 22:17:11 +0100

    Port woff2_common module

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,19 @@
 
 #![allow(non_snake_case)]
 #![allow(non_upper_case_globals)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+#![allow(clippy::needless_range_loop)]
 
 pub mod table_tags;
+pub mod woff2_common;
+
+#[inline(always)]
+pub fn PREDICT_FALSE(cond: bool) -> bool {
+    cond
+}
+
+#[inline(always)]
+pub fn PREDICT_TRUE(cond: bool) -> bool {
+    cond
+}
---
+++ src/woff2_common.rs
@@ -0,0 +1,87 @@
+/* Copyright 2014 Google Inc. All Rights Reserved.
+
+   Distributed under MIT license.
+   See file LICENSE for detail or copy at https://opensource.org/licenses/MIT
+*/
+
+//! Common definition for WOFF2 encoding/decoding
+//! Helpers common across multiple parts of woff2
+
+pub const kWoff2Signature: u32 = 0x774f4632; // "wOF2"
+
+// Leave the first byte open to store flag_byte
+pub const kWoff2FlagsTransform: u32 = 1 << 8; // was "unsigned int" in C
+
+// TrueType Collection ID string: 'ttcf'
+pub const kTtcFontFlavor: u32 = 0x74746366;
+
+pub const kSfntHeaderSize: usize = 12;
+pub const kSfntEntrySize: usize = 16;
+
+pub struct Point {
+    pub x: i32,
+    pub y: i32,
+    pub on_curve: bool,
+}
+
+pub struct Table<'a> {
+    pub tag: u32,
+    pub flags: u32,
+    pub src_offset: u32,
+    pub src_length: u32,
+
+    pub transform_length: u32,
+
+    pub dst_offset: u32,
+    pub dst_length: u32,
+    pub dst_data: &'a [u8],
+}
+
+impl PartialEq for Table<'_> {
+    fn eq(&self, other: &Self) -> bool {
+        self.tag.eq(&other.tag)
+    }
+}
+impl PartialOrd for Table<'_> {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.tag.partial_cmp(&other.tag)
+    }
+}
+
+/// Size of the collection header. 0 if version indicates this isn't a
+/// collection. Ref http://www.microsoft.com/typography/otspec/otff.htm,
+/// True Type Collections
+pub(crate) fn CollectionHeaderSize(header_version: u32, num_fonts: u32) -> usize {
+    let mut size: usize = 0;
+    if header_version == 0x00020000 {
+        size += 12; // ulDsig{Tag,Length,Offset}
+    }
+    if header_version == 0x00010000 || header_version == 0x00020000 {
+        size += 12   // TTCTag, Version, numFonts
+      + 4 * (num_fonts as usize); // OffsetTable[numFonts]
+    }
+    size
+}
+
+/// Compute checksum over size bytes of buf
+pub(crate) fn ComputeULongSum(buf: &[u8], size: usize) -> u32 {
+    let mut checksum: u32 = 0;
+    let aligned_size: usize = size & !3;
+    for i in (0..aligned_size).step_by(4) {
+        checksum += ((buf[i] as u32) << 24)
+            | ((buf[i + 1] as u32) << 16)
+            | ((buf[i + 2] as u32) << 8)
+            | (buf[i + 3] as u32);
+    }
+
+    // treat size not aligned on 4 as if it were padded to 4 with 0's
+    if size != aligned_size {
+        let mut v: u32 = 0;
+        for i in aligned_size..size {
+            v |= (buf[i] as u32) << (24 - 8 * (i & 3));
+        }
+        checksum += v;
+    }
+
+    checksum
+}

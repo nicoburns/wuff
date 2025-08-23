e001a0e0095a3b1051aa79bec8c98fa3a9313c44
Nico Burns <nico@nicoburns.com>
2025-08-23 18:19:35 +0100

    Optimise ComputeULongSum

--- src/woff2_common.rs
+++ src/woff2_common.rs
@@ -65,25 +65,68 @@ pub(crate) fn CollectionHeaderSize(header_version: u32, num_fonts: u32) -> usize
     size
 }
 
+// This function is the direct port of the C ComputeULongSum function.
+// A more optimised version replaces it below.
+//
+// pub(crate) fn ComputeULongSum(buf: &[u8], size: usize) -> u32 {
+//     let mut checksum: u32 = 0;
+//     let aligned_size: usize = size & !3;
+//     for i in (0..aligned_size).step_by(4) {
+//         checksum += ((buf[i] as u32) << 24)
+//             | ((buf[i + 1] as u32) << 16)
+//             | ((buf[i + 2] as u32) << 8)
+//             | (buf[i + 3] as u32);
+//     }
+
+//     // treat size not aligned on 4 as if it were padded to 4 with 0's
+//     if size != aligned_size {
+//         let mut v: u32 = 0;
+//         for i in aligned_size..size {
+//             v |= (buf[i] as u32) << (24 - 8 * (i & 3));
+//         }
+//         checksum += v;
+//     }
+
+//     checksum
+// }
+
 /// Compute checksum over size bytes of buf
 pub(crate) fn ComputeULongSum(buf: &[u8], size: usize) -> u32 {
+    ComputeULongSumSlice(&buf[0..size])
+}
+
+/// Compute checksum over size bytes of buf
+pub(crate) fn ComputeULongSumSlice(buf: &[u8]) -> u32 {
     let mut checksum: u32 = 0;
-    let aligned_size: usize = size & !3;
-    for i in (0..aligned_size).step_by(4) {
-        checksum += ((buf[i] as u32) << 24)
-            | ((buf[i + 1] as u32) << 16)
-            | ((buf[i + 2] as u32) << 8)
-            | (buf[i + 3] as u32);
+    let mut iter = buf.chunks_exact(4);
+    for chunk in &mut iter {
+        // TODO: use
+        checksum += ((chunk[0] as u32) << 24)
+            | ((chunk[1] as u32) << 16)
+            | ((chunk[2] as u32) << 8)
+            | (chunk[3] as u32);
     }
 
-    // treat size not aligned on 4 as if it were padded to 4 with 0's
-    if size != aligned_size {
-        let mut v: u32 = 0;
-        for i in aligned_size..size {
-            v |= (buf[i] as u32) << (24 - 8 * (i & 3));
+    // Treat size not aligned on 4 as if it were padded to 4 with 0's.
+    //
+    // Note(nico): as far as I can tell the zeroes don't actually effect the checksum except in that
+    // they allow the trailing non-aligned bytes to take effect.
+    let remainder = iter.remainder();
+    match remainder {
+        &[a, b, c] => {
+            checksum += ((a as u32) << 24) | ((b as u32) << 16) | ((c as u32) << 8);
         }
-        checksum += v;
-    }
+        &[a, b] => {
+            checksum += ((a as u32) << 24) | ((b as u32) << 16);
+        }
+        &[a] => {
+            checksum += (a as u32) << 24;
+        }
+        [] => {
+            // Do nothing
+        }
+        _ => unreachable!("chunk size was 4 so remainder will be a slice of length 3 or smaller"),
+    };
 
     checksum
 }

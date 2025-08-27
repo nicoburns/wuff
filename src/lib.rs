48b1294ca2d38f926da40a8f28ca37b42c4314d1
Nico Burns <nico@nicoburns.com>
2025-08-27 17:53:38 +0100

    Remove unused parse module

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,6 @@
 pub mod buffer;
 mod decompress;
 mod error;
-mod parse;
 pub mod table_tags;
 mod types;
 pub mod variable_length;
--- src/parse.rs
+++
@@ -1,7 +0,0 @@
-use bytes::Buf;
-
-use crate::error::WuffErr;
-
-pub trait Parse: Sized {
-    fn parse(input: &mut impl Buf) -> Result<Self, WuffErr>;
-}
